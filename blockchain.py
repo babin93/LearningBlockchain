f3305f9dffc9e1cf983e529e643583ebe40e9be8
Marc-Antoine Babin <mababin93@gmail.com>
2017-11-11 19:59:51 -0500

    Blockchain class signatures

---
+++ blockchain.py
@@ -0,0 +1,20 @@
+class BlockChain(object){
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+    
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        #Adds a new transaction to the list of transactions
+        pass
+    
+    @staticmethod
+    def hash(block):
+        # Hashes the block
+        pass
+    
+    
+}
